ff649f4340c54735255085d7bdb16e06a6be107d
Sachin <sachinyadav7398129247@gmail.com>
2024-03-26 23:54:47 +0530

    new files added in interface

---
+++ client/src/interface/FL-144.ts
@@ -0,0 +1,22 @@
+export default interface FL144 {
+    AttorneyOrPartyWithoutAttorney: string;
+    TelephoneNo: string;
+    FaxNo: string;
+    EmailAddress: string;
+    AttorneyFor: string;
+    SUPERIORCOURTOFCALIFORNIA: {
+        Countyof: string;
+        StreetAddress: string;
+        MailingAddress: string;
+        CityAndZipCode: string;
+        BranchName: string;
+    };
+    PlaintiffPetitioner: string;
+    DefendantRespondent: string;
+    Other: string;
+    CaseNumber: string;
+    Date: string;
+    TypeOrPrintName: string;
+    SignatureOfPetitioner: string; 
+    SignatureOfRespondent: string;  
+} 
---
+++ client/src/interface/FL-145Copy.ts
@@ -0,0 +1,16 @@
+export default interface FL145Copy {
+    AttorneyOrPartyWithoutAttorney: string;
+    TelephoneNo: string;
+    AttorneyFor: string;
+    SUPERIORCOURTOFCALIFORNIACOUNTYOF: string;
+    ShortTitle: string;
+    FormInterrogatoriesFamilyLaw: {
+        AskingParty: string;
+        AnsweringParty: string;
+        SetNo: string;
+    };
+    CaseNumber: string;
+    InstructionsToTheAskingParty: boolean[];
+    Date: string;
+    Signature: string;   
+} 
