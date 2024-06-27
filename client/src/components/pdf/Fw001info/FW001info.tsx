fce8b18302c2182583550fc98cd3501a76db6cce
Sv6392 <160205734+Sv6392@users.noreply.github.com>
2024-06-27 12:20:34 +0530

    all work on 26/06/2024

---
+++ client/src/components/pdf/Fw001info/FW001info.tsx
@@ -0,0 +1,154 @@
+import React from 'react'
+
+export default function FW001Info() {
+    return (
+
+        <>
+            <div style={{ maxWidth: "1100px", margin: "auto" }}>
+                <h3 style={{ display: "flex", justifyContent: "end", marginTop: "0", marginBottom: "0" }}>FW-001-INFO</h3>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}><h1><b>INFORMATION SHEET ON WAIVER OF SUPERIOR COURT FEES AND COSTS</b></h1></div><br />
+                <p style={{fontSize:'16px'}}>If you have been sued or if you wish to sue someone, if you are filing or have received a family law petition, or if you are asking the court to appoint a guardian for a minor or a conservator for an adult or are an appointed guardian or conservator, and if you (or your ward or conservatee) cannot afford to pay court fees and costs, you may not have to pay them in order to go to court. If you (or your ward or conservatee) are getting public benefits, are a low-income person, or do not have enough income to pay for your (or his or her) household’s basic needs and your court fees, you may ask the court to waive all or part of those fees.</p>
+                <div style={{ display: "flex", marginTop: 2 }}>
+                    <div style={{ marginLeft: 0 }}>1.</div>
+                    <div style={{ marginLeft: 10,fontSize:'16px' }}>
+                    To make a request to the court to waive your fees in superior court, complete the Request to Waive Court Fees (form FW-001) or, if you are petitioning for the appointment of a guardian or conservator or are an appointed guardian or conservator, complete the Request to Waive Court Fees (Ward or Conservatee) (form FW-001-GC). If you qualify, the court will waive all or part of its fees for the following:
+                    </div>
+                </div><br />
+                <div style={{marginLeft:20, fontSize:'16px'}}>
+                    <div>•	Filing papers in superior court (other than for an appeal in a case with a value of over $25,000)</div>
+                    <div>•	Making and certifying copies</div>
+                    <div>•	Giving notice and certificates</div>
+                    <div>•	Sheriff’s fee to give notice</div>
+                    <div>•	Sending papers to another courtdepartment</div>
+                    <div>•	Reporter’s fee for attendance at hearing or trial, if the court is not electronically recording the proceeding and you request that the court provide an official reporter</div>
+                    <div>•	Assessment for court investigations under Probate Code section 1513, 1826, or 1851</div>
+                    <div>•	Preparing, certifying, copying, and sending the clerk’s transcript on appeal</div>
+                    <div>•	Holding in trust the deposit for a reporter’s transcript on appeal under rule 8.833 or 8.834</div>
+                    <div>•	Making a transcript or copy of an official electronic recording under rule 8.835</div>
+                </div>
+                <div style={{ display: "flex", marginTop: 2 }}>
+                    <div style={{ marginLeft: 0 }}>2.</div>
+                    <div style={{ marginLeft: 10,fontSize:'16px' }}>
+                    You may ask the court to waive other court fees during your case in superior court as well. To do that, complete a <i>Request to Waive Additional Court Fees (Superior Court) (form FW-002) or Request to Waive Additional Court Fees (Superior Court) (Ward or Conservatee) (form FW-002-GC).</i>Request to Waive Additional Court Fees (Superior Court) (form FW-002) or Request to Waive Additional Court Fees (Superior Court) (Ward or Conservatee) (form FW-002-GC).
+                    </div>
+                </div><br />
+                <div style={{marginLeft:20, fontSize:'16px'}}>
+                    <div>•	Jury fees and expenses</div>
+                    <div>•	Fees for a peace officer to testify incourt</div>
+                    <div>•	Fees for court-appointed experts</div>
+                    <div>•	Court-appointed interpreter fees for a witness</div>
+                    <div>•	Other necessary court fees</div>
+                </div>
+                <div style={{ display: "flex", marginTop: 2 }}>
+                    <div style={{ marginLeft: 0 }}>3.</div>
+                    <div style={{ marginLeft: 10,fontSize:'16px' }}>
+                    If you want the Appellate Division of the Superior Court or the Court of Appeal to review an order or judgment against you and you want the court fees waived, ask for and follow the instructions on <i>Information Sheet on Waiver of Appellate Court Fees (Supreme Court, Court of Appeal, Appellate Division) (form APP-015/FW-015-INFO).</i>
+                    </div>
+                </div>
+                <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}><h1><b>IMPORTANT INFORMATION!</b></h1></div><br />
+                <div><b>•	You are signing your request under penalty of perjury. Answer truthfully, accurately, and completely.</b></div>
+                <div><b>•	The court may ask you for information and evidence.</b>You may be ordered to go to court to answer questions about your ability, or the ability of your ward or conservatee, to pay court fees and costs and to provide proof of eligibility. Any initial fee waiver you or your ward or conservatee are granted may be ended if you do not go to court when asked. You or your ward’s or conservatee’s estate may be ordered to repay amounts that were waived if the court finds you were not eligible for the fee waiver.</div>
+                <br />
+                <div><b>•	Public benefits programs listed on the application form.</b>In item 5 on the Request to Waive Court Fees (item 8 of the Request to Waive Court Fees (Ward or Conservatee)), there is a list of programs from which you (or your ward or conservatee) may be receiving benefits, listed by the abbreviations they are commonly known by. The full names of those programs can be found in Government Code section 68632(a), and are also listed here:</div>
+                <div style={{marginLeft:20, fontSize:'16px'}}>
+                    <div>•	Medi-Cal</div>
+                    <div>•	Food Stamps—California Food Assistance Program, CalFresh Program, or SNAP</div>
+                    <div>•	SSP—State Supplemental Payment</div>
+                    <div>•	Supp. Sec. Inc.—Supplemental Security Income (not Social Security)</div>
+                    <div>•  County Relief/Gen. Assist.—County Relief, General Relief (GR), or General Assistance (GA)</div>
+                </div>
+                <br />
+                <hr style={{ width: "100%" }} />
+                <div
+                    className="footer"
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <div style={{ listStyle: "none" }}>
+                        <li>Judicial Council of California,</li>
+                        <li>Revised September 1, 2019 </li>
+                    </div>
+                    <div style={{ textAlign: "center",margin:'5px' }}>
+                        <h3><b>INFORMATION SHEET ON WAIVER OF SUPERIOR COURT FEES AND COSTS</b></h3>
+                    </div>
+                    <div style={{ listStyle: "none" }}>
+                        <li>Government Code, §§ 68630–68640 Cal. Rules of Court, rules 3.51, 7.5</li>
+                        <li>www.courts.ca.gov</li>
+                    </div>
+                    <div style={{ listStyle: "none" }}>
+                        <li>FW-001-INFO, Page 1 of 2</li>
+                    </div>
+                </div>
+                <br /><br />
+                <div style={{marginLeft:20, fontSize:'16px'}}>
+                    <div>•	IHSS—In-Home Supportive Services</div>
+                    <div>•	CalWORKs—California Work Opportunity and Responsibility to Kids Act</div>
+                    <div>•	Tribal TANF—Tribal Temporary Assistance for Needy Families</div>
+                    <div>•	CAPI—Cash Assistance Program for Aged, Blind, or Disabled Legal Immigrants</div>
+                </div>
+                <div><b>•	If you receive a fee waiver, you must tell the court if there is a change in your
+                     finances, or the finances of your ward or conservatee. </b>You must tell the court within
+                      five days if those finances improve or if you, or your ward or conservatee, become able 
+                      to pay court fees or costs during this case. <i>(File Notice to Court of Improved Financial 
+                        Situation or Settlement (form FW-010) or Notice to Court of Improved Financial Situation 
+                        or Settlement (Ward or Conservatee) (form FW-010-GC) with the court.)</i>You may be 
+                        ordered to repay any amounts that were 
+                        waived after your eligibility, or the eligibility of your ward or conservatee, came to anend.</div>
+                <div><b>•	If you receive a judgment or support order in a family law matter: </b>
+                You may be ordered to pay all or part of your waived fees and costs if the court finds your
+                 circumstances have changed so that you can afford to pay. You will have the opportunity to 
+                 ask the court for a hearing if the court makes such a decision.</div>
+                <div><b>•	If you win your case in the trial court: </b>In most circumstances the other 
+                side will be ordered to pay your waived fees and costs to the court. The court will not enter 
+                a satisfaction of judgment until the court is paid. (This does not apply in unlawful detainer 
+                cases. Special rules apply in family law cases and in guardianships and conservatorships. 
+                (Gov. Code, § 68637(d), (e); Cal. Rules of Court, rule 7.5.).</div>
+                <div><b>•	If you settle your civil case for $10,000 or more: </b>Any trial court-waived
+                fees and costs must first be paid to the court out of the settlement.<b> The court will have 
+                a lien on the settlement in the amount of the waived fees and costs. </b> The court may 
+                refuse to dismiss the case until the lien is satisfied. A request to dismiss the case 
+                (use form CIV-110) must have a declaration under penalty of perjury that the waived fees 
+                and costs have been paid. Special rules apply to family law cases.</div>
+                <div><b>•	The court can collect fees and costs due the court.</b>If waived fees and 
+                costs are ordered paid to the trial court, or if you fail to make the payments over time, 
+                the court can start collection proceedings and add a $25 fee plus any additional costs of 
+                collection to the other fees and costs owed to the court.</div>
+                <div><b>•	The fee waiver ends. </b>The fee waiver expires 60 days after the judgment, 
+                dismissal, or other final disposition of the case or earlier if a court finds that you 
+                or your ward or conservatee are not eligible for a fee waiver. If the case is a guardianship 
+                or conservatorship proceeding, see California Rules of Court, rule 7.5(k) for information 
+                on the final disposition of that matter.</div>
+                <div><b>•	If you are in jail or state prison: </b>Prisoners may be required to pay the 
+                full cost of the filing fee in the trial court but may be allowed to do so over time. See 
+                Government Code section 68635.</div>
+                
+                <br /><br />
+                <hr style={{ width: "100%" }} />
+                <div
+                    className="footer"
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                    <div style={{ listStyle: "none" }}>
+                        <li>Judicial Council of California,</li>
+                        <li>Revised September 1, 2019 </li>
+                    </div>
+                    <div style={{ textAlign: "center",margin:'5px' }}>
+                        <h3><b>INFORMATION SHEET ON WAIVER OF SUPERIOR COURT FEES AND COSTS</b></h3>
+                    </div>
+                    <div style={{ listStyle: "none" }}>
+                        <li>Government Code, §§ 68630–68640 Cal. Rules of Court, rules 3.51, 7.5</li>
+                        <li>www.courts.ca.gov</li>
+                    </div>
+                    <div style={{ listStyle: "none" }}>
+                        <li>FW-001-INFO, Page 2 of 2</li>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+
+
+
+
+
+    )
+}
